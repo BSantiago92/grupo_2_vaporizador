9a1e696ae429f8291bb248c86d6568dd0a330c87
bruno <bruno.cami@hotmail.com>
2020-07-24 01:35:41 -0300

    home page y catalogo

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const app = express();
 app.listen(3003, () => {console.log('servidor corriendo en el puerto 3003')})
 
 app.use(express.static('public'));
+app.use(express.static('img'))
 
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/catalogo', function(req,res){
+    res.sendFile(__dirname + '/catalogo.html')
 })
