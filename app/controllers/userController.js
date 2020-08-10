64d2f9a4f8963b7053a02d35a392789dc883603f
bruno <bruno.cami@hotmail.com>
2020-08-10 18:33:27 -0300

    sprint 3

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
      login: (req, res) => {
-        res.sendFile(path.join(__dirname,  '/../views/login.html'))
+        res.render('login')
     },
     
      register: (req, res) => {
