48844570d14d75ddad5ed69c46e56cf33eef7505
bruno <bruno.cami@hotmail.com>
2020-09-09 14:57:27 -0300

    ultimo commit spint 5

--- app/middlewares/adminRoute.js
+++ app/middlewares/adminRoute.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
     if (req.session.user && user.category == "user") {
-        return res.redirect('/user/login')
+        return res.redirect('/user/login');
     }
     next();
 };
